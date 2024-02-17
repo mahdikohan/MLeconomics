f8fc5f4eea707ce93129945a92676c8f02abb1fc
Mahdi Kohan Sefidi <139677886+mahdikohan@users.noreply.github.com>
2024-02-17 21:00:46 +0330

    Update main.py

--- countriesClustring/main.py
+++ countriesClustring/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def get_reg(y):
     """ 
@@ -40,6 +41,7 @@ df_export.drop(columns=df_export.columns[-1], axis=1, inplace=True)
 df_gdp = pd.read_csv('data/GDP_filled_miss_values_knn.csv')
 
 df_gdppc = pd.read_csv('data/GDP_pcap_filled_miss_values_knn.csv')
+df_gdppc.drop(columns=['Unnamed: 67'], axis=1, inplace=True)
 
 df_oilp = pd.read_csv('data/API_NY.GDP.PETR.RT.ZS_DS2_en_csv_v2_6305850.csv')
 df_oilp.drop(columns=df_oilp.columns[-1], axis=1, inplace=True)
@@ -70,52 +72,59 @@ col_rem_gdp = df_gdp.columns
 mt_gdp_log = np.log(df_gdp[all_years].to_numpy())
 
 df_gdp['log_gdp'] = 0.0
-df_gdp['inter'] = 0.0
-df_gdp['beta'] = 0.0
-df_gdp['r2'] = 0.0
+df_gdp['inter_reg_gdp'] = 0.0
+df_gdp['beta_reg_gdp'] = 0.0
+df_gdp['r2_reg_gdp'] = 0.0
 
 for j in range(len(mt_gdp_log)):
     reg = get_reg(mt_gdp_log[j])
-    df_gdp.at[j, 'gdp'] = mt_gdp_log[j][-1]
-    df_gdp.at[j, 'inter'] = reg[0]
-    df_gdp.at[j, 'beta'] = reg[1]
-    df_gdp.at[j, 'r2'] = reg[2]
+    df_gdp.at[j, 'log_gdp'] = mt_gdp_log[j][-1]
+    df_gdp.at[j, 'inter_reg_gdp'] = reg[0]
+    df_gdp.at[j, 'beta_reg_gdp'] = reg[1]
+    df_gdp.at[j, 'r2_reg_gdp'] = reg[2]
 
 df_gdp.drop(labels=col_rem_gdp, axis=1, inplace=True)
 temp2 = pd.concat([df_gdp,temp1], axis=1)
-
 print(temp2)
+
 
 # ---------------------------------
 # Analysis GDP per capita
 mt_gdp_percap_log = np.log(df_gdppc[all_years].to_numpy())
 
-df_gdppc['log_gdp'] = 0.0
-df_gdppc['inter'] = 0.0
-df_gdppc['beta'] = 0.0
-df_gdppc['r2'] = 0.0
+df_gdppc['log_gdp_percap'] = 0.0
+df_gdppc['inter_gdp_percap'] = 0.0
+df_gdppc['beta_gdp_percap'] = 0.0
+df_gdppc['r2_gdp_percap'] = 0.0
 
 for j in range(len(mt_gdp_percap_log)):
     reg = get_reg(mt_gdp_percap_log[j])
-    df_gdppc.at[j, 'gdp'] = mt_gdp_percap_log[j][-1]
-    df_gdppc.at[j, 'inter'] = reg[0]
-    df_gdppc.at[j, 'beta'] = reg[1]
-    df_gdppc.at[j, 'r2'] = reg[2]
+    df_gdppc.at[j, 'log_gdp_percap'] = mt_gdp_percap_log[j][-1]
+    df_gdppc.at[j, 'inter_gdp_percap'] = reg[0]
+    df_gdppc.at[j, 'beta_gdp_percap'] = reg[1]
+    df_gdppc.at[j, 'r2_gdp_percap'] = reg[2]
 
 df_gdppc.drop(labels=all_years, axis=1, inplace=True)
-temp3 = pd.concat([df_gdp,temp1], axis=1)
+temp3 = pd.concat([df_gdppc,temp2], axis=1)
 
 
 # ---------------------------------
 
+temp3 = temp3.drop(['Indicator Code', 'Indicator Name', 'Country Code'], axis=1)
+
+cols = temp3.columns
+cols = cols.drop(['Country Name'])
+print(temp3[cols])
 
 
 
-
+print(temp3[cols].corr())
+sns.heatmap(temp3[cols].corr(), annot=True)
+plt.show()
 # plt.scatter(temp4['gdp'], temp4['beta'])
 # plt.scatter(temp4['inter'], temp4['r2'])
 # plt.scatter(temp4['beta'], temp4['avg oil gdp'])
 # # plt.scatter(beta, r2)
 # # plt.scatter(beta, inter)
 
-plt.show()
+# plt.show()
